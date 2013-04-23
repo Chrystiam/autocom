45535e0a8f837828add54011bac5b0c9e28195c2
Chrystiam <chrystiamgg@misena.edu.co>
2013-04-23 06:19:20 -0500

    commit de prueba con autocomplete en rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Autocomg8::Application.config.secret_token = 'b27688c30824cb7a9653613f13078835e0bb3658f1dc89702994fbe1c622114456f8b84ea667854e44b1831a7b764c337535aeb98d6a4f06ec919c3aee51d2a7'
